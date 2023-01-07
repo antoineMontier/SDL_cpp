34b6551ffe5a8f3e5ab402abd522ac9fd1c59bae
Antoine <antoine.montier@sfr.fr>
2023-01-07 15:02:25 +0100

    added command (events)

--- SDL_test.cpp
+++ SDL_test.cpp
@@ -1,11 +1,6 @@
-
-
 #include "SDL_basics.hpp"
 
 using namespace std;
-
-
-
 
 
 
@@ -17,9 +12,49 @@ int main(){
     std::cout << "SDL window and renderer OK"<<std::endl;
 
     openSDL(WIDTH, HEIGHT, 0, "SDL c++", &w, &r);
+    SDL_SetWindowResizable(w, SDL_TRUE);
     std::cout << "SDL openning OK" << std::endl;
 
-    SDL_Delay(5000);
+    bool program_launched = true;
+
+    while(program_launched){
+        SDL_Event evt; // ask events
+
+        SDL_Delay(100);
+
+        SDL_RenderPresent(r); // refresh the render
+
+        while (SDL_PollEvent(&evt))
+        { // reads all the events (mouse moving, key pressed...)        //possible to wait for an event with SDL_WaitEvent
+            switch (evt.type)
+            {
+            case SDL_WINDOWEVENT:
+                if (evt.window.event == SDL_WINDOWEVENT_RESIZED) {
+                    printf("MESSAGE:Resizing window...\n");
+                    //resizeWindow(evt.window.data1, evt. window.data2);
+                }
+                break;
+            case SDL_QUIT:
+                program_launched = SDL_FALSE; // quit the program if the user closes the window
+                break;
+
+            case SDL_KEYDOWN: // SDL_KEYDOWN : hold a key            SDL_KEYUP : release a key
+                switch (evt.key.keysym.sym)
+                { // returns the key ('0' ; 'e' ; 'SPACE'...)
+
+                case SDLK_ESCAPE:
+                    program_launched = SDL_FALSE; // escape the program by pressing esc
+                    break;
+
+                default:
+                    break;
+                }
+            default:
+                break;
+            }
+        }
+
+    }
 
 
 
